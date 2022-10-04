24f4d83f201471d0114ed44fb865257983f1ed70
aashkul <aashish.kulkarni33@gmail.com>
2022-10-04 16:42:45 +1300

    .gitignore updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,30 @@ import {
   BrowserRouter as Router,
   Redirect
 } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-            <ul className="header">
-                <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-            </ul>
-          <div className="content">
-              <Route exact path = "/" component={Home} />
-              <Route path = "/SEPractice"  component={(SEPractice)} /> 
-              <Route path = "/SubmitArticle" component={SubmitArticle} />
-              <Route exact path = "/404" component= {NotFoundPage} />
-              <Redirect to = "/404" />
-          </div>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <ul className="header">
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/SEPractice" component={(SEPractice)} />
+          <Route path="/SubmitArticle" component={SubmitArticle} />
+          <Route exact path="/404" component={NotFoundPage} />
+          <Redirect to="/404" />
         </div>
-        </Router>
-    );
+      </div>
+    </Router>
+  );
 }
- 
+
 export default App;
