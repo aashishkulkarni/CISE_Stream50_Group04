72daa506959eafdf477d63479fd55490235b5d34
aashkul <aashish.kulkarni33@gmail.com>
2022-10-11 13:18:07 +1300

    Heading fixed

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
- 
-const Home = () =>  {
-    return (
-      <div>
-        <h2> How to use SEED</h2>
-        <p>I am not sure what to put on the home page</p>
-        <p>A nice logo? Instructions?</p>
-      </div>
-    );
-  }
- 
+
+const Home = () => {
+  return (
+    <div>
+      <h2> How to use SPEED</h2>
+      <p>I am not sure what to put on the home page</p>
+      <p>A nice logo? Instructions?</p>
+    </div>
+  );
+}
+
 export default Home;
