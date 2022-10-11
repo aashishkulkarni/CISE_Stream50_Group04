b099129511d1f7a2435067cde3b1f72050a8495a
aashkul <aashish.kulkarni33@gmail.com>
2022-10-11 13:50:13 +1300

    Updated the Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,13 @@ const Home = () => {
   return (
     <div>
       <h1> What is SPEED ?</h1>
-      <h3><u> Introduction </u></h3>
-      <p>A nice logo? Instructions?</p>
+      <h3> Introduction </h3>
+      <p> SPEED is a website which shows the various articles submitted by the user. These articles are then verified by the moderators who either accept or reject the article.
+        'React.js' has been used for designing the front-end aspects of the website
+        'Node.js' is implemented to handle the back-end aspects and 'MongoDB' has been used to handle the database.
+      </p>
+      <h3> Purpose </h3>
+      <p>It is a searchable database of evidence about different claims about different SE practices.</p>
     </div>
   );
 }
